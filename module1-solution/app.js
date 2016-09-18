5a4981968c9d0c1147055d735ebba87381147865
eilsoft <eilsoft@eilsoft.com>
2016-09-18 15:46:35 +0300

    assignment 1

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,7 +6,39 @@ angular.module('LunchCheck', [])
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
+  $scope.dishes = "";
+  $scope.feedbackMsg = "";
+  $scope.statusColor = "";
 
+  $scope.checkLunch = function () {
+    var msg, color;
+    var noOfDishes = getNumberOfDishes($scope.dishes);
+    if (noOfDishes == 0){
+      msg = "Please enter data first"
+      color = "error"
+    } else {
+      color = "success"
+      if (noOfDishes <= 3){
+        msg = "Enjoy!";
+      } else {
+          msg = "Too much!!";
+      }
+    }
+    $scope.feedbackMsg = msg;
+    $scope.statusColor = color;
+   };
 }
-
 })();
+
+function getNumberOfDishes(dishes){
+  var noOfDishes = 0;
+  var dishesArr = dishes.split(",");
+    if (dishes != ""){
+        for (index in dishesArr){
+          if (dishesArr[index].trim() != "") {
+            noOfDishes++;
+          }
+        }
+    }    
+    return noOfDishes;
+}
